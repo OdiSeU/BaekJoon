d91d046522840fca1fd388d361a2d6ccaaf6f332
OdiSeU <secret36565@gmail.com>
2020-11-14 15:07:01 +0900

    201113
    c++
    4179 done

--- Baekjoon_C++/Baekjoon_C++/4179.cpp
+++ Baekjoon_C++/Baekjoon_C++/4179.cpp
@@ -1,28 +1,80 @@
 #include <iostream>
-
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-
 using namespace std;
+struct Coord {
+    int row, col;
+};
+vector<vector<char>> maze;
+int rows, cols;
+int vy[] = { -1, 0 , 1, 0 };
+int vx[] = { 0 , -1, 0, 1 };
 
-long long solution(int n, vector<int> times) {
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> endTimes;
-    for (int i = 0; i < times.size(); i++) {
-        endTimes.push({ times[i], i });
+queue<Coord> fireBFS(queue<Coord> fire) {
+    queue<Coord> result;
+    while (!fire.empty()) {
+        Coord now = fire.front(); fire.pop();
+        for (int i = 0; i < 4; i++) {
+            int r = now.row + vy[i], c = now.col + vx[i];
+            if (0 <= r && r < rows && 0 <= c && c < cols) {
+                if (maze[r][c] != '#' && maze[r][c] != 'F') {
+                    maze[r][c] = 'F';
+                    result.push({ r,c });
+                }
+            }
+        }
     }
-    while (--n) {
-        pair<long long, int> top = endTimes.top();
-        endTimes.pop();
-        top.first += times[top.second];
-        endTimes.push(top);
+    return result;
+}
+queue<Coord> meBFS(queue<Coord> me) {
+    queue<Coord> result;
+    while (!me.empty()) {
+        Coord now = me.front(); me.pop();
+        for (int i = 0; i < 4; i++) {
+            int r = now.row + vy[i], c = now.col + vx[i];
+            if (0 <= r && r < rows && 0 <= c && c < cols) {
+                if (maze[r][c] == '.') {
+                    maze[r][c] = 'J';
+                    result.push({ r,c });
+                }
+            }
+            else {
+                queue<Coord> esc;
+                esc.push({ -1, -1 });
+                return esc;
+            }
+        }
     }
-    return endTimes.top().first;
+    return result;
 }
 
 int main() {
-    int n = 6;
-    vector<int> times = { 7, 10 };
-    cout << solution(n, times);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    queue<Coord> fire, me;
+    bool isEscape = false;
+    int time = 0;
+    cin >> rows >> cols;
+    for (int i = 0; i < rows; i++) {
+        string s;
+        cin >> s;
+        maze.push_back(vector<char>(cols));
+        for (int j = 0; j < s.length(); j++) {
+            maze[i][j] = s[j];
+            if (s[j] == 'J') me.push({ i, j });
+            if (s[j] == 'F') fire.push({ i, j });
+        }
+    }
+    while (!me.empty()) {
+        time++;
+        fire = fireBFS(fire);
+        me = meBFS(me);
+        if (me.front().row == -1) {
+            isEscape = true;
+            break;
+        }
+    }
+    if (isEscape) cout << time;
+    else cout << "IMPOSSIBLE";
+    return 0;
 }
