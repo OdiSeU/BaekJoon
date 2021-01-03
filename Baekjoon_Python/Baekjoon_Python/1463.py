821e4967e225c204c6c62bdb8d6a21345d53b08d
OdiSeU <secret36565@gmail.com>
2021-01-03 16:47:38 +0900

    210103
    python
    1463 done

---
+++ Baekjoon_Python/Baekjoon_Python/1463.py
@@ -0,0 +1,59 @@
+import sys
+import time
+from queue import Queue
+
+import sys
+
+def toOne(n):
+    if n < 2: return 0
+    return min(toOne(n//2) + n%2, toOne(n//3) + n%3)+1
+
+sys.stdout.write(str(toOne(int(sys.stdin.readline()))))
+
+# def toOne(n):
+#     if n < 2: return 0
+#     return min(toOne(n//2) + n%2, toOne(n//3) + n%3)+1
+#
+# def toOne2(n):
+#     memoi = [0] * (N + 1)
+#
+#     for i in range(2, N + 1):
+#         memoi[i] = memoi[i - 1] + 1
+#         if i % 2 == 0:
+#             memoi[i] = min(memoi[i // 2] + 1, memoi[i])
+#         if i % 3 == 0:
+#             memoi[i] = min(memoi[i // 3] + 1, memoi[i])
+#     return memoi[n]
+#
+# def toOne3(n):
+#     visited = [0] * (n + 1)
+#     que = Queue()
+#     que.put([n, 0])
+#     while not que.empty():
+#         now = que.get()
+#         if now[0] == 1:
+#             return now[1]
+#         if not visited[now[0]-1]:
+#             visited[now[0] - 1] = 1
+#             que.put([now[0] - 1, now[1] + 1])
+#         if now[0] % 2 == 0 and not visited[now[0] // 2]:
+#             visited[now[0] // 2] = 1
+#             que.put([now[0] // 2, now[1] + 1])
+#         if now[0] % 3 == 0 and not visited[now[0] // 3]:
+#             visited[now[0] // 3] = 1
+#             que.put([now[0] // 3, now[1] + 1])
+#
+#
+# N = int(sys.stdin.readline())
+#
+# start = time.time()
+# sys.stdout.write(str(toOne(N))+' ')
+# print(time.time()-start)
+#
+# start = time.time()
+# sys.stdout.write(str(toOne2(N))+' ')
+# print(time.time()-start)
+#
+# start = time.time()
+# sys.stdout.write(str(toOne3(N))+' ')
+# print(time.time()-start)
