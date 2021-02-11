6006e98118d29f0be303f5332b5612e2f03eb027
OdiSeU <secret36565@gmail.com>
2021-02-11 23:44:45 +0900

    210211
    17298 done

---
+++ Baekjoon_C++/Baekjoon_C++/17298.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N, A;
+    stack<pair<int, int>> stk;
+    cin >> N;
+    vector<int> nge(N, 0);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A;
+        while (stk.size() && stk.top().first < A)
+        {
+            nge[stk.top().second] = A;
+            stk.pop();
+        }
+        stk.push({A, i});
+    }
+    for (int i = 0; i < N; i++) {
+        cout << (nge[i] ? nge[i] : -1) << ' ';
+    }
+    return 0;
+}
