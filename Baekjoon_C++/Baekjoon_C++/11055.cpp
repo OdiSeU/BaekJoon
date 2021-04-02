d0c6ca1e28aea8528aec5ebbb861259e9cbc751f
OdiSeU <secret36565@gmail.com>
2021-04-02 13:41:19 +0900

    210402
    11055 done

---
+++ Baekjoon_C++/Baekjoon_C++/11055.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+	int N, max = 0;
+	cin >> N;
+	vector<int> seq(N, 0), dp(N, 0);
+	for (int i = 0; i < N; i++) {
+		cin >> seq[i];
+		for (int j = 0; j < i; j++) {
+			if (seq[j] < seq[i] && dp[i] < dp[j]) dp[i] = dp[j];
+		}
+		dp[i] += seq[i];
+		if (max < dp[i]) max = dp[i];
+	}
+	cout << max;
+}
