a8d8220fff5647f269aba82ed695536f70efe19b
OdiseU <secret36565@gmail.com>
2022-05-13 17:37:59 +0900

    220513
    1100 done

---
+++ Baekjoon_Python/Baekjoon_Python/1100.py
@@ -0,0 +1,10 @@
+import sys
+board = [sys.stdin.readline() for _ in range(8)]
+answer = 0
+
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2==0 and board[i][j] == 'F':
+            answer += 1
+
+print(answer)
