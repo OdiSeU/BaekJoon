e7b389428ee2f2363878da9d45a835997282b72b
OdiseU <secret36565@gmail.com>
2022-06-03 12:02:45 +0900

    220603
    2669 done

---
+++ Baekjoon_Python/Baekjoon_Python/2669.py
@@ -0,0 +1,9 @@
+area = set()
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            area.add(x*100+y)
+
+print(len(area))
