99a46a8b1c30962de3fe3e090b077e484fe68cf8
OdiseU <secret36565@gmail.com>
2022-07-04 12:57:27 +0900

    220704
    17425 done

---
+++ Baekjoon_Python/Baekjoon_Python/17425.py
@@ -0,0 +1,16 @@
+import sys
+
+input()
+
+nums = list(map(int, sys.stdin.readlines()))
+MAX = max(nums)
+
+dp = [0] * (MAX+1)
+for i in range(1, MAX+1):
+    for j in range(i, MAX+1, i):
+        dp[j] += i
+
+for i in range(1, MAX+1):
+    dp[i] += dp[i-1]
+
+print('\n'.join(map(lambda x: str(dp[x]), nums)))
