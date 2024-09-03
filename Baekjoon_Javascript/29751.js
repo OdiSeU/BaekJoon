f58c9951467c1fd3ad6ea41b07abd8b90cf0378d
OdiseU <binarycastle98@gmail.com>
2024-09-03 17:01:22 +0900

    240903

---
+++ Baekjoon_Javascript/29751.js
@@ -0,0 +1,6 @@
+const path = process.platform === 'linux' ? 0 : `${__dirname}/input.txt`;
+const file = require('fs').readFileSync(path, 'utf-8').trim();
+
+const [W, H] = file.split(' ').map(Number);
+
+console.log(((W * H) / 2).toFixed(1));
