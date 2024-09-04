14cc0b92881e2bc7a7db1821fb8b1d014d1138cd
OdiseU <binarycastle98@gmail.com>
2024-09-04 12:26:39 +0900

    240904

--- Baekjoon_Javascript/3845.js
+++ Baekjoon_Javascript/3845.js
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/3845
+
 const path = process.platform === 'linux' ? 0 : `${__dirname}/input.txt`;
 const file = require('fs').readFileSync(path, 'utf-8').trim();
 
