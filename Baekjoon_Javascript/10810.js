43cc6907be36703f08412eaa60517510c9893213
OdiseU <binarycastle98@gmail.com>
2024-09-09 10:36:15 +0900

    240909

--- Baekjoon_Javascript/10810.js
+++ Baekjoon_Javascript/10810.js
@@ -1,6 +1,21 @@
+// https://www.acmicpc.net/problem/10810
+
 const path = process.platform === 'linux' ? 0 : `${__dirname}/input.txt`;
 const file = require('fs').readFileSync(path, 'utf-8').trim();
+const lines = file.split('\n').map((line) => line.split(' ').map(Number));
 
-const [S, i] = file.split('\n');
+const [[N, M], ...inputs] = lines;
 
-console.log(S[+i - 1]);
+console.log(solution(N, M, inputs));
+
+function solution(N, M, inputs) {
+    const arr = Array(N).fill(0);
+
+    for (const [from, to, x] of inputs) {
+        for (let i = from - 1; i < to; i++) {
+            arr[i] = x;
+        }
+    }
+
+    return arr.join(' ');
+}
---
+++ Baekjoon_Javascript/27866.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/27866
+
+const path = process.platform === 'linux' ? 0 : `${__dirname}/input.txt`;
+const file = require('fs').readFileSync(path, 'utf-8').trim();
+
+const [S, i] = file.split('\n');
+
+console.log(S[+i - 1]);
